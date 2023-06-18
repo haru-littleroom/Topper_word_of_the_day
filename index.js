6841aca206c1ea078df50b7111372c9b34fda8d8
haru_littleroom <haru.littleroom@gmail.com>
2023-06-18 17:24:20 +0900

    20230618：trial upload to main

---
+++ index.js
@@ -0,0 +1,18 @@
+// Get all the images
+image_array = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg'
+  ]
+  
+  function get_random_image(){
+    // Get a random index
+    random_index = Math.floor(Math.random() * image_array.length);
+  
+    // Get an image at the random_index
+    selected_image = image_array[random_index]
+  
+    // Display the image
+    document.getElementById('image_shower').src = `./images/${selected_image}`
+  }
